b93b6823b87133e026d5511d64a9c7d91158eab9
Decoder4code <gameanimator2004@gmail.com>
2023-12-25 20:41:14 +0530

    coding the app

--- episode_5_talk_is_cheap_show_me_code/app.js
+++ episode_5_talk_is_cheap_show_me_code/app.js
@@ -18,23 +18,47 @@ import ReactDOM from "react-dom/client";
         -copyrights
     */
     const Title =()=>(
-        <h1 id="title" key="h2">
-            Food Villa
-        </h1>
+        <a href="/">
+        <img 
+            className="logo"
+            alt="logo"
+            src="https://t3.ftcdn.net/jpg/03/74/73/78/360_F_374737839_VsxlmnN7soqOOOFFzy5kfE9H6PFMCkPX.jpg"
+        />
+        </a>
     );
     const Headercomponent=()=>(
-            <div>
+            <div className="header">
                 <Title />     
-                <h2>Namaste React functional component</h2>
-                <h2>This is h2 tag</h2>
+                <div className="nav-items">
+                    <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    </ul>
+                </div>
             </div>
     );
 
-        const AppLayout=() =>(
-            {
-                
-            }
+    const Body=() =>{
+        return(
+            <h4>Body</h4>
         );
+    };
+    const Footer=() =>{
+        return(
+            <h4>Footer</h4>
+        );
+    };
+        const AppLayout=() =>{
+            return(
+                <>
+                <Header/>
+                <Body/>
+                <Footer/>
+                </>
+            );
+        };
 
 
 
